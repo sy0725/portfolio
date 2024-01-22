550d4503db1d24b7637051e90e4048275410c381
Jeon SunYong <sunyong0725@naver.com>
2024-01-22 13:35:55 +0900

    🌈 테스트용 임시 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import "@/style/globals.css";
 export const metadata: Metadata = {
   title: "SunYong Portfolio",
   description: "커뮤니케이션으로 성장을 즐기는 프론트엔드 개발자 전선용입니다.",
+  icons : {
+    icon : '/public/favicon/favicon.ico'
+  },
+  keywords : '전선용 포트폴리오'
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import "@/style/globals.css";
 
 function page() {
   return (
-    <div>
+    <div className="bg-red-900">
       하이 시작이지 해봐야지 이쁘게 만들어보자아아아 !!
-      <p>dasdasdasddasd</p>;
+      <p className="bg-red-400">dasdasdasddasd</p>
     </div>
   );
 }
