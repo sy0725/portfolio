a21c557d65c2484a02bda1013a07da19fb283c95
Jeon SunYong <sunyong0725@naver.com>
2024-01-22 15:03:15 +0900

    🌈 Hello 1차 수정 - fixed 완료 추가할 내용 주석 처리

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,42 @@
 'use client'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Light from '@images/lightTheme.svg';
+import Dark from '@images/darkTheme.svg';
 
-import React, { useState } from 'react'
-import Image  from 'next/image';
-import Light from '@images/lightTheme.svg'
-import Dark from '@images/darkTheme.svg'
+// 헤더 부분 앞으로 추가할 부분
+// 다크모드 , 라이트모드
+// 버튼 클릭시 페이지 이동 - About : About , Project : Project Link로 연결하여 이동하기
 
 function Header() {
-  // 나중에 zustand 사용해서 바꿀 예정
-  const [isButtonClicked, setIsButtonClicked] = useState(false)
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
   const toggleButtonImage = () => {
-    setIsButtonClicked(!isButtonClicked)
-  }
+    setIsButtonClicked(!isButtonClicked);
+  };
+
   return (
-    <div>
-      <div className='flex justify-between px-[60px] pt-5'>
-      <p className='mobile:text-[24px] tablet:text-[28px] desktop:text-[32px]'>JSY Portfolio</p>
-      <button type='button' onClick={toggleButtonImage}>
-      <Image
-      src={isButtonClicked ? Light : Dark}
-      width={30}
-      height={30}
-      alt='모드 변경 버튼'
-      />
-      </button>
+    <>
+      <div className='fixed w-full'>
+        <div className='flex justify-between px-[60px] pt-5'>
+          <p className='mobile:text-[24px] tablet:text-[28px] desktop:text-[32px]'>JSY Portfolio</p>
+          <div className='flex gap-[60px] mobile:text-[24px] tablet:text-[28px] desktop:text-[32px]'>
+            <button type='button'>About</button>
+            <button type='button'>Project</button>
+            <button className='pb-[5px]' type='button' onClick={toggleButtonImage}>
+              <Image
+                src={isButtonClicked ? Light : Dark}
+                width={30}
+                height={30}
+                alt='모드 변경 버튼'
+              />
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default Header
+export default Header;
+
