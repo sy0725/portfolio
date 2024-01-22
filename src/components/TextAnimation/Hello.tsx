591060fd00397b9d75c6971804bf7207dab2b672
Jeon SunYong <sunyong0725@naver.com>
2024-01-22 14:30:08 +0900

    🌈 Hello 1차 수정 - 반응형

---
+++ src/components/TextAnimation/Hello.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hello() {
+  return (
+    <div className='desktop:text-[64px] mobile:text-[44px] tablet:text-[54px] font-bold text-black leading-[1.5] pl-[60px] flex items-center  h-screen'>
+      안녕하세요. <br/>
+      프론트엔드 개발자 <br/>
+      전선용 입니다.
+    </div>
+    
+  );
+}
+
+export default Hello;
