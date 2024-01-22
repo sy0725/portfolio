0ef4a9953019824fb91f4541c4957f9edaa6b41d
Jeon SunYong <sunyong0725@naver.com>
2024-01-22 15:03:36 +0900

    🌈 Hello 1차 수정 - 추가할 부분 주석 처리

--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// 추가할 부분
+// 텍스트 애니메이션
+
+
 function Hello() {
   return (
     <div className='desktop:text-[64px] mobile:text-[44px] tablet:text-[54px] font-bold text-black leading-[1.5] pl-[60px] flex items-center  h-screen'>
