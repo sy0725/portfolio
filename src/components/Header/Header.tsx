e8c43d40237b2e66af7365c91cb182adc0bfb495
Jeon SunYong <sunyong0725@naver.com>
2024-01-22 14:10:47 +0900

    🌈 Header 1차 수정

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React, { useState } from 'react'
+import Image  from 'next/image';
+import Light from '@images/lightTheme.svg'
+import Dark from '@images/darkTheme.svg'
+
+function Header() {
+  // 나중에 zustand 사용해서 바꿀 예정
+  const [isButtonClicked, setIsButtonClicked] = useState(false)
+
+  const toggleButtonImage = () => {
+    setIsButtonClicked(!isButtonClicked)
+  }
+  return (
+    <div>
+      <div className='flex justify-between px-[60px] pt-5'>
+      <p className='text-[32px]'>JSY Portfolio</p>
+      <button type='button' onClick={toggleButtonImage}>
+      <Image
+      src={isButtonClicked ? Light : Dark}
+      width={30}
+      height={30}
+      alt='맞을겨 ?'
+      />
+      </button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
