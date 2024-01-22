6ac2d84ea5bff102dd5d4fb745fa5f12768cdfb3
Jeon SunYong <sunyong0725@naver.com>
2024-01-22 14:40:40 +0900

    🌈 기본 셋팅 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "@/style/globals.css";
-
+import Header from "@/components/Header/Header";
+import Hello from "@/components/TextAnimation/Hello";
 function page() {
   return (
-    <div className="bg-red-900">
-      하이 시작이지 해봐야지 이쁘게 만들어보자아아아 !!
-      <p className="bg-red-400">dasdasdasddasd</p>
+    <div>
+      <Header/>
+      <Hello/>
     </div>
   );
 }
