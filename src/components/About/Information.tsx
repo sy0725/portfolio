a0dfacdaa678121238ae1d0662eefc9c0bca58ca
Jeon SunYong <sunyong0725@naver.com>
2024-01-23 21:12:42 +0900

    🌈 애니메이션 삭제 , 반응형 수정

--- src/components/About/Information.tsx
+++ src/components/About/Information.tsx
@@ -1,35 +1,14 @@
-"use client";
-
-import { Variants, easeInOut, motion } from "framer-motion";
 import INFO_LIST from "@/constants/INFO_LIST";
 
 function Information() {
-  const motionVariants: Variants = {
-    begin: {
-      x: -100,
-      opacity: 0,
-      filter: "blur(50px)",
-    },
-    finish: {
-      x: 0,
-      opacity: 1,
-      filter: "blur(0px)",
-      transition: {
-        duration: 0.5,
-        delay: 0.3,
-        ease: easeInOut,
-      },
-    },
-  };
-
   const renderALinkOrSpan = (text: string) => {
     return text.includes("https" || "http") ? (
       <a
         href={text}
         target="_blank"
         rel="noopener noreferrer"
-        className="underline underline-offset-[5px]"
-      >
+        className="underline 
+        underline-offset-[1px]">
         {text}
       </a>
     ) : (
@@ -39,25 +18,14 @@ function Information() {
 
   return (
     <div className="flex gap-6 items-center justify-center">
-      <motion.ul
-        initial="begin"
-        animate="finish"
-        variants={motionVariants}
-        className="list-none p-0 m-0"
-      >
-        {INFO_LIST.map(({ icon, text }, i) => (
-          <motion.li
-            key={i}
-            initial="begin"
-            animate="finish"
-            variants={motionVariants}
-            className="mobile:text-[18px] mobile:leading-3 tablet:text-[28px] tablet:leading-5 flex gap-5 items-center justify-start pb-2 leading-7"
-          >
+      <ul>
+        {INFO_LIST.map(({icon, text}, i) => (
+          <li key={i} className="mobile:text-[18px] mobile:leading-3 tablet:text-[28px] tablet:leading-5 flex gap-5 items-center justify-start pb-2 leading-7">
             <span>{icon}</span>
             {renderALinkOrSpan(text)}
-          </motion.li>
+          </li>
         ))}
-      </motion.ul>
+      </ul>
     </div>
   );
 }
