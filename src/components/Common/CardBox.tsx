a535d5eaee08a8598caa982525e2386e6a8152a8
Jeon SunYong <sunyong0725@naver.com>
2024-01-23 21:54:59 +0900

    🌈 CardBox 속성 추가

--- src/components/Common/CardBox.tsx
+++ src/components/Common/CardBox.tsx
@@ -1,18 +1,20 @@
 interface Props {
   children: React.ReactNode;
   className?: string;
+  flex? : string
 }
 
 function CardBox({
   children,
   className = "",
+  flex = 'flex-row',
   ...props
 }: Props) {
   return (
     <>
       <div
         className={`
-      border-[3px] rounded-[20px] min-w-[1510px] max-w-[1510px] mobile:min-w-[600px] mobile:max-w-[600px] tablet:min-w-[1100px] tablet:max-w-[1100px] h-auto flex shadow-md justify-around py-[100px] ${className}`}
+      border-[3px] rounded-[20px] min-w-[1510px] max-w-[1510px] mobile:min-w-[600px] mobile:max-w-[600px] tablet:min-w-[1100px] tablet:max-w-[1100px] h-auto flex shadow-md justify-around py-[100px] ${flex} ${className}`}
         {...props}>
         {children}
       </div>
