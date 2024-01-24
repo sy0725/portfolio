b88caf10ebf81fafc6a7ad22076c3068e9e701fe
Jeon SunYong <sunyong0725@naver.com>
2024-01-24 15:35:47 +0900

    🌈 Hello framer-motion 삭제

--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,39 +1,15 @@
 'use client'
 
-import { useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
 import Animation from './Animation';
 
 function Hello() {
-  const textArray = ['안녕하세요.', '프론트엔드 개발자', '전선용입니다.'];
-  const controls = useAnimation();
-
-  const animateText = async () => {
-    while (true) {
-      for (let i = 0; i < textArray.length; i++) {
-        await controls.start({ opacity: 1, transition: { duration: 0.5 } });
-        await new Promise((resolve) => setTimeout(resolve, 500)); 
-        await controls.start({ opacity: 0, transition: { duration: 0.5 } });
-        await new Promise((resolve) => setTimeout(resolve, 500));
-      }
-    }
-  };
-
-  useEffect(() => {
-    animateText();
-  }, [controls]);
-
+ 
   return (
-    <div className='flex justify-around'>
-    <div className='desktop:text-[64px] mobile:text-[44px] tablet:text-[54px] font-bold text-black leading-[1.5] pl-[60px] flex items-center h-screen'>
-      <motion.div animate={controls} style={{ opacity: 0 }}>
-        {textArray.map((text, index) => (
-          <div key={index}>
-            {text}
-            <br />
-          </div>
-        ))}
-      </motion.div>
+    <div className='flex justify-around desktop:text-[64px] mobile:text-[44px] tablet:text-[50px] font-bold text-black leading-[1.5]'>
+    <div className='flex items-center h-screen'>
+    안녕하세요. <br/>
+      프론트엔드 개발자 <br/>
+      전선용 입니다.
     </div>
     <Animation/>
     </div>
