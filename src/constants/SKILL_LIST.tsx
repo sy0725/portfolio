7b62c41cd282949be8805bc036c03fd47b4d2233
Jeon SunYong <sunyong0725@naver.com>
2024-01-24 14:43:09 +0900

    🌈 SKILL_LIST 추가

---
+++ src/constants/SKILL_LIST.tsx
@@ -0,0 +1,34 @@
+export type SkillProps = {
+  src : string;
+  alt : string
+}
+
+const SKILL_LIST = [
+  { src: '/images/About/Skill/html.png', alt: 'HTML' },
+  { src: '/images/About/Skill/css.png', alt: 'CSS' },
+  { src: '/images/About/Skill/tailwindcss.png', alt: 'Tailwind CSS' },
+  { src: '/images/About/Skill/javascript.png', alt: 'JavaScript' },
+  { src: '/images/About/Skill/react.png', alt: 'React' },
+  { src: '/images/About/Skill/framer-motion.png', alt: 'Framer Motion' },
+];
+
+
+export const LEARN_LIST = [
+  { src: '/images/About/Skill/nextjs.png', alt: 'Next-JS' },
+  { src: '/images/About/Skill/swr.png', alt: 'SWR' },
+  { src: '/images/About/Skill/typescript.png', alt: 'TypeScript' },
+  { src: '/images/About/Skill/styledcomponent.png', alt: 'styledcomponent' },
+  { src: '/images/About/Skill/zustand.png', alt: 'Zustand' },
+  { src: '/images/About/Skill/react-query.png', alt: 'React-Query' },
+]
+
+export const COOPORATION_LIST = [
+  { src: '/images/About/Skill/github.png', alt: 'Github' },
+  { src: '/images/About/Skill/notion.png', alt: 'Notion' },
+]
+
+export const BACKEND_LIST = [
+  { src: '/images/About/Skill/pocket-base.png', alt: 'PocketBase' },
+  { src: '/images/About/Skill/supabase.png', alt: 'SupaBase' },
+]
+export default SKILL_LIST
