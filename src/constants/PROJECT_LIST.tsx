ac35ba25e15bd51b3de65c6cf4a93950c0ef7374
Jeon SunYong <sunyong0725@naver.com>
2024-01-26 12:01:45 +0900

    🌈 PROJECT_LIST 임시 추가

---
+++ src/constants/PROJECT_LIST.tsx
@@ -0,0 +1,16 @@
+const PROJECT_LIST = [
+  {
+    id : 1,
+    title : 'JUNGLE',
+    ImageSource : [
+      // video로 할지 고민좀
+    ],
+    core : '반응형 UI 제작. Framer-Motion을 활용한 애니메이션 구현. Supabase를 이용한 데이터 구축. Zustand를 이용한 애니메이션 상태 관리. Supabase 와 Zustand를 활용한 "글 추가 , 삭제 기능" , "댓글 추가 , 삭제 기능". React-Scroll 라이브러리를 이용한 섹션 애니메이션 구현.',
+    new : 'Styled - Component 라는 새로운 "CSS-in-JS" 라이브러리를 알게 되어 다양한 스타일링을 경험하고 학습했습니다. Framer-Motion의 애니메이션도 분리하여 관리가 가능하다는걸 경험했습니다. Supabase를 프로젝트에서 활용하기 위해 설정들을 공부하고 구축 할 수 있었습니다. TypeScript를 활용한 첫번째 장기 프로젝트 였습니다. ➡️ 많은 TypeError를 경험했고, 그 경험을 통해 해결 방법을 학습했습니다. Zustand 상태관리 라이브러리가 다른 라이브러리들의 사용법 보다 훨씬 더 쉽다는걸 경험했습니다.',
+    URL : {
+      깃허브 : 'https://github.com/twelive/JUNGLE',
+      베포 : 'https://jungler.vercel.app/'
+    },
+    
+  },
+]
