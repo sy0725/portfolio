484abe32c20d759aec460e16707731eea79d9ca7
Jeon SunYong <sunyong0725@naver.com>
2024-01-26 12:03:12 +0900

    🌈 ErrorPage 크기 조절 추후 가운데로 이동

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -6,12 +6,14 @@ import lottieError from 'public/animation/error.json'
 
 function error() {
   return (
+    <div className='flex justify-center items-center'>
     <Lottie
       loop
       animationData={lottieError}
       play
       style={{ width: '31.25rem', height: '31.25rem' }}
     />
+    </div>
   )
 }
 
